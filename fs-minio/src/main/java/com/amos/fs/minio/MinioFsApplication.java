b00a0cb27df13b507fe94585bc14d57fb3984808
amos.wang <amos.wang>
2020-12-15 18:15:25 +0800

    feat: init fs minio

---
+++ fs-minio/src/main/java/com/amos/fs/minio/MinioFsApplication.java
@@ -0,0 +1,13 @@
+package com.amos.fs.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioFsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinioFsApplication.class, args);
+    }
+
+}
