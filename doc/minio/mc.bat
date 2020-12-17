347d48db20a6c6809fd90d1e5e72822a0b5ce27b
amos.wang <1833063210@qq.com>
2020-12-17 23:40:25 +0800

    feat: fs-minio windows shell

---
+++ doc/minio/mc.bat
@@ -0,0 +1,3 @@
+mc config host add minio http://127.0.0.1:9000 amos 12345678 --api s3v4
+
+mc policy set public minio/amos
---
+++ doc/minio/run.bat
@@ -0,0 +1,5 @@
+set MINIO_ACCESS_KEY=amos
+set MINIO_SECRET_KEY=12345678
+
+minio server E:\app_res\minio\resource
+
